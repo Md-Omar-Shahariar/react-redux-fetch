53d9f8db519c16b32acd3bcf85469943e875ff31
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-07-17 15:50:49 +0600

    react-redux-fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
       <h1>React Redux Fetch</h1>
+      <Todos></Todos>
     </div>
   );
 }
 
 export default App;
+
+//step1: install package
+//step2: constant define
+//step3: async action creator
+//step4: reducer
+//step5: create  Store
+//step6: provide Store
+//step7: use Store
+//
